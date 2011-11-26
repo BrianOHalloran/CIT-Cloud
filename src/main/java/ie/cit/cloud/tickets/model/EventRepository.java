17fc0472f203c33220958c846fd91e4c7d876421
Brian O'Halloran <BrianOHalloran@gmail.com>
2011-11-26 02:27:28 +0000

    local H2 repository with Hibernate/JPA
    need to populate data now

--- src/main/java/ie/cit/cloud/tickets/model/EventRepository.java
+++ src/main/java/ie/cit/cloud/tickets/model/EventRepository.java
@@ -1,7 +1,7 @@
 package ie.cit.cloud.tickets.model;
 
-//import javax.persistence.EntityManager;
-//import javax.persistence.PersistenceContext;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 import ie.cit.cloud.tickets.model.performance.Event;
 import ie.cit.cloud.tickets.model.performance.Location;
@@ -9,25 +9,16 @@ import ie.cit.cloud.tickets.model.performance.Performer;
 
 import java.util.Date;
 import java.util.List;
+import javax.persistence.Query;
 
-//import org.hibernate.Query;
-//import org.hibernate.Session;
-//import org.hibernate.SessionFactory;
-//import org.springframework.beans.factory.annotation.Autowired;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 @Repository("hibernateEventRepository")
 public class EventRepository implements IEventRepository
 {
 
-//	 @PersistenceContext
-//	 private EntityManager em;
-
-	@Autowired
-	private SessionFactory sf;
+	 @PersistenceContext
+	 private EntityManager em;
 
 	public EventRepository()
 	{
@@ -37,13 +28,13 @@ public class EventRepository implements IEventRepository
 	@Override
 	public Performer getPerformer(final String performerName)
 	{
-//		final Query query = session().createQuery("from Performer p where p.name = :performerName");
-//		query.setString("performerName", performerName);
-//		final Object result = query.uniqueResult();
-//		if(result != null && result instanceof IPerformer)
-//		{
-//			return (IPerformer)result;
-//		}
+		final Query query = em.createQuery("from Performer p where p.name = :performerName");
+		query.setParameter("performerName", performerName);
+		final Object result = query.getSingleResult();
+		if(result != null && result instanceof Performer)
+		{
+			return (Performer)result;
+		}
 		return null;
 	}
 
@@ -51,70 +42,57 @@ public class EventRepository implements IEventRepository
 	@Override
 	public List<Performer> getPerformers()
 	{
-		//return em.createQuery("from Performer").getResultList();
-		return (List<Performer>)session().createQuery("from Performer").list();
+		return em.createQuery("from Performer").getResultList();
 	}
 
 	@Override
 	public Location getLocation(final String locationName)
 	{
-//		final Query query = session().createQuery("from Location l where l.name = :locationName");
-//		query.setString("locationName", locationName);
-//		final Object result = query.uniqueResult();
-//		if(result != null && result instanceof ILocation)
-//		{
-//			return (ILocation)result;
-//		}
+		final Query query = em.createQuery("from Location l where l.name = :locationName");
+		query.setParameter("locationName", locationName);
+		final Object result = query.getSingleResult();
+		if(result != null && result instanceof Location)
+		{
+			return (Location)result;
+		}
 		return null;
 	}
 
+	@SuppressWarnings("unchecked")
 	@Override
 	public List<Location> getLocations()
 	{
-		return null;
-//		return em.createQuery("from Location").getResultList();
-//		return (List<ILocation>)session().createQuery("Location").list();
+		return em.createQuery("from Location").getResultList();
 	}
 
 	@Override
 	public Event getEvent(final Performer performer, final Location location, final Date date)
 	{
-//		final Query query = session().createQuery("from Event e where e.location.name = :locationName and e.performer.name = :performerName" and e.date = :date");
-//		query.setString("locationName", location.getName());
-//		query.setString("performerName", performer.getName());
-//		query.setString("date", date.toString());
-//		final Object result = query.uniqueResult();
-//		if(result != null && result instanceof IEvent)
-//		{
-//			return (IEvent)result;
-//		}
+		final Query query = em.createQuery("from Event e where e.location.name = :locationName and e.performer.name = :performerName and e.date = :date");
+		query.setParameter("locationName", location.getName());
+		query.setParameter("performerName", performer.getName());
+		final Object result = query.getSingleResult();
+		if(result != null && result instanceof Event)
+		{
+			return (Event)result;
+		}
 		return null;
 	}
 
+	@SuppressWarnings("unchecked")
 	@Override
 	public List<Event> getEvents(final Performer performer, final Location location)
 	{
-//		final Query query = session().createQuery("from Event e where e.location.name = :locationName and e.performer.name = :performerName");
-//		query.setString("locationName", location.getName());
-//		query.setString("performerName", performer.getName());
-//		final Object result = query.list();
-//		if(result != null)
-//		{
-//			return (List<IEvent>)result;
-//		}
-		return null;
+		final Query query = em.createQuery("from Event e where e.location.name = :locationName and e.performer.name = :performerName");
+		query.setParameter("locationName", location.getName());
+		query.setParameter("performerName", performer.getName());
+		return query.getResultList();
 	}
 
+	@SuppressWarnings("unchecked")
 	@Override
 	public List<Event> getEvents()
 	{
-		return null;
-//		return em.createQuery("from Event").getResultList();
-//		return (List<IEvent>)session().createQuery("Event").list();
-	}
-
-	private Session session()
-	{
-		return sf.getCurrentSession();
+		return em.createQuery("from Event").getResultList();
 	}
 }
